7854f3a5980030668faaa3bd966908c90fed85d2
Eduardzsosa72 <eduardzsosa72@gmail.com>
2026-06-02 23:05:31 +0000

    xdd

--- config/db.php
+++ config/db.php
@@ -1,14 +1,21 @@
 <?php
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'taller_pmv');
-
+require_once __DIR__ . '/../vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
 function getConn() {
-    $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-    if ($conn->connect_error) {
-        die(json_encode(['error' => 'Conexión fallida: ' . $conn->connect_error]));
+    static $conn = null;
+
+    if ($conn === null) {
+        $conn = new PDO(
+            "mysql:host=" . $_ENV['DB_HOST'] .
+            ";port=" . $_ENV['DB_PORT'] .
+            ";dbname=" . $_ENV['DB_NAME'] .
+            ";charset=utf8mb4",
+            $_ENV['DB_USER'],
+            $_ENV['DB_PASS']
+        );
+
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
-    $conn->set_charset('utf8mb4');
     return $conn;
 }
